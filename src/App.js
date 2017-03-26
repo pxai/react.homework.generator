19eeca31a6d27c4639a05bdb693bb40dce81f0b7
pxai <pello_altadill@cuatrovientos.org>
2017-03-26 15:51:52 +0200

    Updated Father from child component

--- src/App.js
+++ src/App.js
@@ -7,24 +7,33 @@ import ExerciseGroup from './ExerciseGroup';
 var App = React.createClass({
   getDefaultProps: function () {
       return {
-        title: 'React Homework Generator'
+        title: 'React Homework Generator',
+        name: 'June'
       };
   },
-
+  // State can be changed
   getInitialState: function () {
-    return {};
+    return {
+        title: 'React Homework Generator',
+        name: 'June'
+    };
   },
-
+  handleNewName: function (name) { // Called from the child component
+      this.setState({
+            name : name
+      });
+      console.log('Called from child: ' + name);
+  },
   render: function () {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>React HomeWork Generator</h2>
+          <h2>React HomeWork Generator {this.state.name}</h2>
         </div>
-        <FormGenerate />
+        <FormGenerate onNewName={this.handleNewName}/>
         <p className="App-intro">
-          <ExerciseGroup  />
+        <ExerciseGroup  />
         </p>
       </div>
     );
--- src/FormGenerate.js
+++ src/FormGenerate.js
@@ -5,6 +5,7 @@ var FormGenerate = React.createClass({
   // Props are set once
   getDefaultProps: function () {
       return {
+        who: 'Student',
         pages: 1,
         difficulty: 2
       };
@@ -13,20 +14,23 @@ var FormGenerate = React.createClass({
   // State can be changed
   getInitialState: function () {
     return {
+      who: this.props.who,
       pages: this.props.pages,
       difficulty: this.props.difficulty
     };
   },
-
   onButtonClick: function (e) {
     e.preventDefault();
         // We do this way to update and to force re-rendering
         if (typeof this.refs.pages.value === 'string' && this.refs.pages.value !== '') {
           this.setState({
+            who: this.refs.who.value,
             pages : this.refs.pages.value,
             difficulty : this.refs.difficulty.value
           });
+          this.props.onNewName(this.refs.who.value,);
         }
+          this.refs.who.value = '';
           this.refs.pages.value = '';
           this.refs.difficulty.value = '';
   },
@@ -35,6 +39,8 @@ var FormGenerate = React.createClass({
     return (
       <div className="FormGenerate">
         <form onSubmit={this.onButtonClick}>
+          <label>Who</label><br />
+          <input type="text" ref="who"  /><br />
           <label>Pages</label><br />
           <input type="text" ref="pages"  /><br />
           <label>Difficulty</label><br />
